a9ae1d45380e3292370f220e8512fa8fa3946a51
Doris Elisa Martinez <nmdelisa@gmail.com>
2025-10-02 22:40:22 -0400

    tarea capitulo 4

---
+++ capitulo 4/programa 4.2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Cubo-2
+El programa calcula el cubo de los 10 primeros numeros naturales con la ayuda de una funcion. */
+
+int cubo (void);   /* prototipo de funcion. */
+int I;             /* variable global. */
+
+void main (void)
+{
+    int CUB;
+    for (I = 1; I <= 10; I++)
+    {
+        CUB = cubo();   /*llamada a la funcion cubo. */
+         printf("\nEl cubo de %d es: %d", I, CUB);
+    }
+}
+
+int cubo(void)       /*declaracion de la funcion. */
+/* La funcion calcula el cubo de la variable local I. */
+{
+    int I = 2;             /* Variable local entera I con el mismo nombre que la variable global. */
+    return (I*I*I);
+}
